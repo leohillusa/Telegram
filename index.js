18568f45db1acf9b5a8c72d5c4619231a32b9bb9
leohillusa <leohillusa@gmail.com>
2025-12-05 04:02:22 -0500

    Initialize Express server in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running ✔️");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
